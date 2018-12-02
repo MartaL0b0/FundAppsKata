c8292f817cb2e4f48974de64c447747b75a508fb
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-12-02 11:03:38 +0000

    define tests for req 1

--- src/test/PlutoRoverTest.java
+++ src/test/PlutoRoverTest.java
@@ -1,5 +1,53 @@
 package test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 
 public class PlutoRoverTest {
 
+	@Test
+	public void testInitEmpty() {
+		
+	}
+	
+	@Test
+	public void testInitParameters() {
+		
+	}
+	
+	@Test
+	public void testCurrentPosition() {
+		
+	}
+	
+	@Test
+	public void testMoveForwardNorth() {
+		
+	}
+	
+	@Test
+	public void testMoveForwardSouth() {
+		
+	}
+	
+	@Test
+	public void testMoveForwardEast() {
+		
+	}
+	
+	@Test
+	public void testMoveForwardWest() {
+		
+	}
+	
+	@Test
+	public void testMoveForwardEdgeNS() {
+		
+	}
+	
+	@Test
+	public void testMoveForwardEdgeEW() {
+		
+	}
 }
