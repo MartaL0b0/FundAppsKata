df0640795a737f650434dd3442b7e46761f48530
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-12-02 11:13:29 +0000

    Test for constructors

--- src/test/PlutoRoverTest.java
+++ src/test/PlutoRoverTest.java
@@ -3,17 +3,45 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import source.PlutoRover;
+
 
 public class PlutoRoverTest {
 
 	@Test
 	public void testInitEmpty() {
-		
+		PlutoRover rover = new PlutoRover();
+		assertEquals(0, rover.getX_coord());
+		assertEquals(0, rover.getY_coord());
+		assertEquals('N', rover.getOrientation());
+		assertEquals(100, rover.getMapSize());
 	}
 	
 	@Test
 	public void testInitParameters() {
-		
+		PlutoRover rover = new PlutoRover(5, 5, 'S', 40);
+		assertEquals(5, rover.getX_coord());
+		assertEquals(5, rover.getY_coord());
+		assertEquals('S', rover.getOrientation());
+		assertEquals(40, rover.getMapSize());
+	}
+	
+	@Test
+	public void testInitParametersExceptionNegativeCoord() {
+		try{
+			PlutoRover rover = new PlutoRover(5, -5, 'S', 40);
+		}catch (Exception e){
+			assertEquals("Position cannnot be negative", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testInitParametersExceptionInvalidOrientation() {
+		try{
+			PlutoRover rover = new PlutoRover(5, 5, 'M', 40);
+		}catch (Exception e){
+			assertEquals("Orientation not valid", e.getMessage());
+		}
 	}
 	
 	@Test
